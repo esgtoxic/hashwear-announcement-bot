bded86bb3d2eae392644fa62778e20e99e222c08
esgtoxic <manavgoyal2008@gmail.com>
2026-09-05 04:37:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hashwear Announcement Bot is running.');
+}).listen(PORT, '0.0.0.0', () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
 require('dotenv').config();
 
 const {
